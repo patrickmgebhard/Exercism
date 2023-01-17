208baaf1bc848b63b09eed23d8bac2e4c35a672e
Patrick Gebhard <patrickmgebhard@gmail.com>
2023-01-17 16:13:27 +0100

    doing arcade game exercise

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -3,16 +3,10 @@
 Learn about Guido, the creator of the Python language: https://en.wikipedia.org/wiki/Guido_van_Rossum
 """
 
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
-
 EXPECTED_BAKE_TIME = 40
-
-# TODO: consider defining the 'PREPARATION_TIME' constant
-#       equal to the time it takes to prepare a single layer
 
 PREPARATION_TIME = 2
 
-# TODO: define the 'bake_time_remaining()' function
 def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
 
@@ -25,14 +19,9 @@ def bake_time_remaining(elapsed_bake_time):
     """
     return EXPECTED_BAKE_TIME - elapsed_bake_time
 
-# TODO: define the 'preparation_time_in_minutes()' function
-#       and consider using 'PREPARATION_TIME' here
-
 def preparation_time_in_minutes(number_of_layers):
     """Calculate the preparation time"""
     return number_of_layers * PREPARATION_TIME
-
-# TODO: define the 'elapsed_time_in_minutes()' function
 
 def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
     """Calculate the total elapsed time"""
